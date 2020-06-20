80c19f50e7fa6a693e1385e6c63d8b372695be72
daniel <daniel@bogusemailserver.com>
2020-06-20 16:10:16 +0200

    added Stream type

--- lib/Streaming/Chunked.hs
+++ lib/Streaming/Chunked.hs
@@ -1,8 +1,13 @@
 {-# LANGUAGE ImportQualifiedPost #-}
-module Streaming.Chunked () where
+{-# LANGUAGE StandaloneKindSignatures #-}
+module Streaming.Chunked (
+        Stream
+    ) where
 
-import Streaming
+import Data.Kind
+import Streaming qualified as S
 import Streaming.Prelude qualified as S
 import Streaming.Chunked.Chunk qualified as C
 
-
+type Stream :: (Type -> Type) -> Type -> Type
+data Stream m r = Stream { getStream :: S.Stream (S.Of C.Chunk) m r }
